5fb499f5b9c4e45ae696ddae2a2545b5529d177f
amir58 <devamir11@gmail.com>
2022-08-19 09:52:11 +0200

    Initial commit

---
+++ lib/NewsResponse.dart
@@ -0,0 +1,147 @@
+/// status : "ok"
+/// totalResults : 16
+/// articles : [{"source":{"id":null,"name":"Youm7.com"},"author":null,"title":"أسعار الذهب ترتفع 15 جنيها بالتعاملات المسائية.. وعيار 21 يسجل 1080 جنيها - اليوم السابع","description":"شهدت أسعار الذهب مستهل التعاملات المسائية اليوم الثلاثاء ارتفاعا ملحوظا بحوالى 15 جنيها للجرام، حيث تحرك جرام الذهب عيار 21 وهو الأكثر مبيعا من 1065 جنيها للجرام إلى 1080 جنيها.","url":"https://www.youm7.com/story/2022/8/16/أسعار-الذهب-ترتفع-15-جنيها-بالتعاملات-المسائية-وعيار-21-يسجل/5873185","urlToImage":"https://img.youm7.com/large/202203220713151315.jpg","publishedAt":"2022-08-16T19:00:00Z","content":null},{"source":{"id":null,"name":"Newsmisr.info"},"author":null,"title":"500 ميجا في الثانية.. «ستارلينك» تكشف موعد تطبيق باقة الإنترنت الفضائي في مصر - نيوز مصر","description":"أعلنت شركة ستارلينك التابعة لرجل الأعمال الأمريكي إيلون ماسك، اعتزامها إطلاق خدمة الانترنت الفضائي في مصر عام 2023، مؤكدة أنها ستلبي جميع احتياجات ورغبات","url":"https://www.newsmisr.info/2022/08/16/500-%d9%85%d9%8a%d8%ac%d8%a7-%d9%81%d9%8a-%d8%a7%d9%84%d8%ab%d8%a7%d9%86%d9%8a%d8%a9-%d8%b3%d8%aa%d8%a7%d8%b1%d9%84%d9%8a%d9%86%d9%83-%d8%aa%d9%83%d8%b4%d9%81-%d9%85%d9%88%d8%b9%d8%af/","urlToImage":"https://www.newsmisr.info/wp-content/uploads/2022/08/spacex-starlink-asteroid-hunting.jpg","publishedAt":"2022-08-16T16:54:06Z","content":"2023 .\r\n500 110 2090 .\r\n « » :\r\n<ul><li> «Starlink internet:   110 2090 50 250 / 599 11381 .</li><li> «Starlink RV»: 135 2565 50 250 / 599 11381 .</li><li> «Starlink Business »: 500 2500 150-250 / 25… [+58 chars]"},{"source":{"id":null,"name":"Masrawy.com"},"author":"مصراوي","title":"تويوتا كورولا بـ900 ألف وهيونداي توسان بمليون.. إلى أين وصل الأوفر برايس؟ - Masrawy - مصراوي","description":"تويوتا كورولا بـ900 ألف وهيونداي توسان بمليون إلى أين وصل الأوفر برايس | مصراوى","url":"https://www.masrawy.com/autos/autos_news/details/2022/8/16/2276002/تويوتا-كورولا-بـ900-ألف-وهيونداي-توسان-بمليون-إلى-أين-وصل-الأوفر-برايس-","urlToImage":"https://media.gemini.media/img/large/2021/11/14/2021_11_14_16_13_53_617.jpg","publishedAt":"2022-08-16T16:14:00Z","content":"- :\r\n \" \" \" \" \" \" .\r\n \" \" 400 .\r\n 5% .\r\n 5 50%:"},{"source":{"id":null,"name":"Investing.com"},"author":"Investing.com","title":"عاجل: الأسهم تهتز بعد مفأجاة وول مارت.. ومعركة الذهب والدولار تشتعل بواسطة Investing.com - السعودية Investing.com","description":"عاجل: الأسهم تهتز بعد مفأجاة وول مارت.. ومعركة الذهب والدولار تشتعل","url":"https://sa.investing.com/news/stock-market-news/article-2308079","urlToImage":"https://i-invdn-com.investing.com/news/LYNXNPEC0E0NG_L.jpg","publishedAt":"2022-08-16T15:07:00Z","content":"%USER_NAME%\r\n %USER_NAME% Investing.com."},{"source":{"id":null,"name":"Asharqbusiness.com"},"author":"اقتصاد الشرق مع Bloomberg","title":"مصدران لـ\"الشرق\": \"مدينة نصر\" المصرية ترفض السماح لـ\"سوديك\" بإجراء الفحص النافي للجهالة - اقتصاد الشرق مع Bloomberg","description":"قال مصدران لـ\"الشرق\" إن مساهمي شركة مدينة نصر للإسكان رفضوا خلال اجتماع الجمعية العامة العادية للشركة السماح لشركة السادس من ...","url":"https://www.asharqbusiness.com/article/40728","urlToImage":"https://cdn.asharqbusiness.com/RAC.php?src=14779932631659949536.jpg&w=830&h=550","publishedAt":"2022-08-16T14:04:20Z","content":"\"\" () .\r\n \"\" 100% 6.18 3.30 .\r\n \" \" \" \" .\r\n\" \" \"\" \r\n 15% \" \" \" \" 20% \" \" 7.45% .\r\n 2.40 3.6% .\r\n\" \" 6 15%\r\n \" \" 35% 9.5 \"\" .\r\n6 .. \" \" \"\" \r\n 2% ."},{"source":{"id":"google-news","name":"Google News"},"author":null,"title":"سعر الدولار مقابل الجنيه المصرى فى منتصف تعاملات اليوم الثلاثاء - Al Masry Al Youm - المصري اليوم","description":null,"url":"https://news.google.com/__i/rss/rd/articles/CBMiMmh0dHBzOi8vd3d3LmFsbWFzcnlhbHlvdW0uY29tL25ld3MvZGV0YWlscy8yNjY2ODk50gEA?oc=5","urlToImage":null,"publishedAt":"2022-08-16T13:21:26Z","content":null},{"source":{"id":null,"name":"Skynewsarabia.com"},"author":"سكاي نيوز عربية","title":"قبل القرار بيومين.. إليك توقعات الخبراء لقرار الفائدة في مصر - Sky News Arabia سكاي نيوز عربية","description":"تترقب الأوساط المالية في مصر قرار البنك المركزي يوم الخميس المقبل الخاص بمعدلات الفائدة، في وقت يعمل على كبح معدلات التضخم.","url":"https://www.skynewsarabia.com/business/1547221-%D8%A7%D9%84%D9%82%D8%B1%D8%A7%D8%B1-%D8%A8%D9%8A%D9%88%D9%85%D9%8A%D9%86-%D8%A7%D9%95%D9%84%D9%8A%D9%83-%D8%AA%D9%88%D9%82%D8%B9%D8%A7%D8%AA-%D8%A7%D9%84%D8%AE%D8%A8%D8%B1%D8%A7%D8%A1-%D9%84%D9%82%D8%B1%D8%A7%D8%B1-%D8%A7%D9%84%D9%81%D8%A7%D9%8A%D9%94%D8%AF%D8%A9-%D9%85%D8%B5%D8%B1","urlToImage":"https://www.skynewsarabia.com/images/v1/2022/08/16/1547224/1200/630/1-1547224.jpg","publishedAt":"2022-08-16T12:43:58Z","content":"13.6 13.2 1.3 0.1 .\r\n 15.6 14.6 . .\r\n .\r\n 2022 22 14 21 300 11.25% 12.25% 11.75%.\r\n\" \": 1 .\r\n 2 .\r\n .\r\n .\r\n 20 .\r\n  \" \" \" \" 1 -1.5 .\r\n .\r\n \" \" 22 24 \" \" 19.15 .\r\n .\r\n 3 1 23 .\r\n . \"\" .\r\n \" \" .\r\n3.8 4… [+40 chars]"},{"source":{"id":"google-news","name":"Google News"},"author":null,"title":"انهيار تاريخي في أسعار الذهب اليوم | تفاصيل - Sada El-Bald صدى البلد","description":null,"url":"https://news.google.com/__i/rss/rd/articles/CBMiIGh0dHBzOi8vd3d3LmVsYmFsYWQubmV3cy81Mzk5NTUw0gEA?oc=5","urlToImage":null,"publishedAt":"2022-08-16T08:19:00Z","content":null},{"source":{"id":null,"name":"Alarabiya.net"},"author":"العربية","title":"صندوق الاستثمارات العامة يشتري حصصاً في شركات أميركية كبرى - العربية","description":"اشترى صندوق الاستثمارات العامة السعودي (PIF)، حصص أقلية في سلسلة من الشركات الأميركية الكبرى بما في ذلك ألفابت وجي بي مورغان وAmerican Tower Corporation وأ","url":"https://www.alarabiya.net/aswaq/special-stories/2022/08/16/%D8%B5%D9%86%D8%AF%D9%88%D9%82-%D8%A7%D9%84%D8%A7%D8%B3%D8%AA%D8%AB%D9%85%D8%A7%D8%B1%D8%A7%D8%AA-%D8%A7%D9%84%D8%B9%D8%A7%D9%85%D8%A9-%D9%8A%D8%B4%D8%AA%D8%B1%D9%8A-%D8%AD%D8%B5%D8%B5%D8%A7-%D9%81%D9%8A-%D8%B4%D8%B1%D9%83%D8%A7%D8%AA-%D8%A3%D9%85%D9%8A%D8%B1%D9%83%D9%8A%D8%A9-%D9%83%D8%A8%D8%B1%D9%89","urlToImage":"https://vid.alarabiya.net/images/2022/05/18/aee3701f-c32f-4f9c-8b1b-a1dd9f487857/aee3701f-c32f-4f9c-8b1b-a1dd9f487857_16x9_600x338.jpg","publishedAt":"2022-08-16T06:53:00Z","content":"(PIF) American Tower Corporation .\r\n 464 433.6 JPMorgan Chase 507 Zoom .\r\n 620 .\r\n 7.1% 40.7 \"\".\r\n Meta Platforms 2.9 418000 474 .\r\n 16 4.7 507.2 1.9% 1.04 Costco Wholesale Corp 496.8 .\r\n 6.37 493.3 … [+145 chars]"},{"source":{"id":null,"name":"Newsmisr.info"},"author":null,"title":"لو عايز تبني.. أسعار الحديد والأسمنت اليوم الثلاثاء 16 أغسطس 2022 في سوق البناء - نيوز مصر","description":"شهدت أسعار مواد البناء المكونة من الحديد والأسمنت خلال بداية تعاملات اليوم الثلاثاء الموافق 16 أغسطس 2022 في كافة المصانع والشركات في الأسواق داخل مصر،","url":"https://www.newsmisr.info/2022/08/16/%d9%84%d9%88-%d8%b9%d8%a7%d9%8a%d8%b2-%d8%aa%d8%a8%d9%86%d9%8a-%d8%a3%d8%b3%d8%b9%d8%a7%d8%b1-%d8%a7%d9%84%d8%ad%d8%af%d9%8a%d8%af-%d9%88%d8%a7%d9%84%d8%a3%d8%b3%d9%85%d9%86%d8%aa-%d8%a7%d9%84%d9%8a/","urlToImage":"https://www.newsmisr.info/wp-content/uploads/2022/08/IMG_٢٠٢٢٠٨١٦_٠٥٥٠١٧.jpg","publishedAt":"2022-08-16T06:10:19Z","content":null},{"source":{"id":null,"name":"Alarabiya.net"},"author":"العربية","title":"أكبر شركة في العالم تحدد نظام العمل الجديد لموظفيها بدءاً من سبتمبر - العربية","description":"استدعت شركة أبل موظفيها في مقاطعة سانتا كلارا بالقرب من مقر الشركة في كاليفورنيا للعودة للعمل من المكتب بدءاً من سبتمبر، حيث من المتوقع أن يعملوا 3 أيام أس","url":"https://www.alarabiya.net/aswaq/special-stories/2022/08/16/%D8%A3%D9%83%D8%A8%D8%B1-%D8%B4%D8%B1%D9%83%D8%A9-%D9%81%D9%8A-%D8%A7%D9%84%D8%B9%D8%A7%D9%84%D9%85-%D8%AA%D8%AD%D8%AF%D8%AF-%D9%86%D8%B8%D8%A7%D9%85-%D8%A7%D9%84%D8%B9%D9%85%D9%84-%D8%A7%D9%84%D8%AC%D8%AF%D9%8A%D8%AF-%D9%84%D9%85%D9%88%D8%B8%D9%81%D9%8A%D9%87%D8%A7-%D8%A8%D8%AF%D8%A7%D9%8A%D8%A9-%D9%85%D9%86-%D8%B3%D8%A8%D8%AA%D9%85%D8%A8%D8%B1","urlToImage":"https://vid.alarabiya.net/images/2022/03/04/70d8c93f-9216-4327-ab19-cfd56a1a3cba/70d8c93f-9216-4327-ab19-cfd56a1a3cba_16x9_600x338.jpg","publishedAt":"2022-08-16T04:59:00Z","content":"3 \"\" \"CNBC\" \"The Verge\" \".\".\r\n . .\r\n .\r\n . 3 .\r\n \" \" The Verge."},{"source":{"id":null,"name":"Alarabiya.net"},"author":"العربية","title":"إيلون ماسك يكتب مقالاً للصين عن رؤيته للمستقبل.. بينها مدينة على المريخ - العربية","description":"ساهم الرئيس التنفيذي لشركة تسلا، إيلون ماسك بكتابة عمود رأي في الإصدار الرسمي لمجلة إدارة الفضاء الإلكتروني في الصين، وهي الوكالة ذات النفوذ التي تشرف على","url":"https://www.alarabiya.net/aswaq/special-stories/2022/08/16/%D8%A7%D9%8A%D9%84%D9%88%D9%86-%D9%85%D8%A7%D8%B3%D9%83-%D9%8A%D9%83%D8%AA%D8%A8-%D9%85%D9%82%D8%A7%D9%84%D8%A7%D9%8B-%D9%84%D9%84%D8%B5%D9%8A%D9%86-%D8%B9%D9%86-%D8%B1%D8%A4%D9%8A%D8%AA%D9%87-%D8%A7%D9%84%D9%85%D8%B3%D8%AA%D9%82%D8%A8%D9%84%D9%8A%D8%A9-%D8%A8%D9%8A%D9%86%D9%87%D8%A7-%D9%85%D8%AF%D9%8A%D9%86%D8%A9-%D8%B9%D9%84%D9%89-%D8%A7%D9%84%D9%85%D8%B1%D9%8A%D8%AE","urlToImage":"https://vid.alarabiya.net/images/2022/07/07/c956b914-ddae-44be-a349-0d8504691c13/c956b914-ddae-44be-a349-0d8504691c13_16x9_600x338.jpg","publishedAt":"2022-08-16T04:52:00Z","content":".\r\n CAC .\r\n 2020 CAC.\r\n .\r\n Trivium China : \" - . \". \" \".\r\n . \" \" . .\r\n . .\r\n : \" . \".\r\n - - . Neuralink Corp Bot . \" \" .\r\n : \" . \"."},{"source":{"id":null,"name":"Youm7.com"},"author":null,"title":"الدولار هو السبب.. أسعار الذهب تتراجع وعيار 21 يسجل 1050 جنيها.. فيديو - اليوم السابع","description":"قدم تلفزيون اليوم السابع تغطية خاصة حول أسعار الذهب، وقال نادى نجيب عضو شعبة الذهب بالغرف التجارية، إن الذهب طوال الوقت غير مستقر لأنه سلعة عالمية..","url":"https://www.youm7.com/story/2022/8/16/الدولار-هو-السبب-أسعار-الذهب-تتراجع-وعيار-21-يسجل-1050/5872097","urlToImage":"https://img.youm7.com/large/202208151135473547.jpg","publishedAt":"2022-08-16T02:00:00Z","content":null},{"source":{"id":null,"name":"Muhtwaplus.com"},"author":"جابر السويفي","title":"أرخص سيارة زيرو موديل 2023 في مصر..\"سعر يبهرك\" - محتوى بلس","description":"يبحث الكثيرون من الراغبين في شراء سيارة عن أرخص سيارة زيرو موديل 2023 في مصر، من أجل شرائها لمساعدتهم في حياتهم اليومية، بالإضافة إلى التخلص من زحمة المواصلات. ماهي أرخص سيارة زيرو موديل 2023 في مصر تعتبر السيارة لادا جرانتا من أرخص السيارات موديل 2023 في الس…","url":"https://muhtwaplus.com/271175/2022/08/16/%d8%a3%d8%b1%d8%ae%d8%b5-%d8%b3%d9%8a%d8%a7%d8%b1%d8%a9-%d8%b2%d9%8a%d8%b1%d9%88-%d9%85%d9%88%d8%af%d9%8a%d9%84-2023-%d9%81%d9%8a-%d9%85%d8%b5%d8%b1-%d8%b3%d8%b9%d8%b1-%d9%8a%d8%a8%d9%87%d8%b1%d9%83/","urlToImage":"https://cdn.muhtwaplus.com/wp-content/uploads/95.jpg","publishedAt":"2022-08-15T22:58:24Z","content":null},{"source":{"id":null,"name":"Muhtwaplus.com"},"author":"نور أحمد","title":"سعر الفائدة على شهادات البنك الأهلي .. اعرفها قبل اجتماع المركزي المقبل - محتوى بلس","description":"يبحث الكثير من الأشخاص، عن سعر الفائدة على شهادات البنك الأهلي المصري، وذلك قبل اجتماع لجنة السياسة النقدية في البنك المركزي المصري، يوم الخميس المقبل الموافق 18 أغسطس الحالي، لمناقشة أسعار الفائدة. سعر الفائدة على شهادات البنك الأهلي المصري: 1-الشهادة البلات…","url":"https://muhtwaplus.com/270930/2022/08/15/%d8%b3%d8%b9%d8%b1-%d8%a7%d9%84%d9%81%d8%a7%d8%a6%d8%af%d8%a9-%d8%b9%d9%84%d9%89-%d8%b4%d9%87%d8%a7%d8%af%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d9%86%d9%83-%d8%a7%d9%84%d8%a3%d9%87%d9%84%d9%8a-%d8%a7%d8%b9%d8%b1%d9%81%d9%87%d8%a7-%d9%82%d8%a8%d9%84-%d8%a7%d8%ac%d8%aa%d9%85%d8%a7%d8%b9-%d8%a7%d9%84%d9%85%d8%b1%d9%83%d8%b2%d9%8a-%d8%a7%d9%84%d9%85%d9%82%d8%a8%d9%84/","urlToImage":"https://cdn.muhtwaplus.com/wp-content/uploads/2021/11/5-2.jpg","publishedAt":"2022-08-15T13:00:24Z","content":null},{"source":{"id":null,"name":"Masrawy.com"},"author":"مصراوي","title":"في 10 بنوك.. رسوم السحب والاستعلام عن الرصيد عبر أي ماكينة ATM - Masrawy - مصراوي","description":"في 10 بنوك رسوم السحب والاستعلام عن الرصيد عبر أي ماكينة atm | مصراوى","url":"https://www.masrawy.com/news/news-banking/details/2022/8/15/2275344/في-10-بنوك-رسوم-السحب-والاستعلام-عن-الرصيد-عبر-أي-ماكينة-atm-","urlToImage":"https://media.gemini.media/img/large/2020/10/25/2020_10_25_12_37_23_760.jpg","publishedAt":"2022-08-15T13:00:00Z","content":"- :\r\n (ATM) .\r\n 27 2020 2022 .\r\n 5 .\r\n 4 .\r\n ATM 10 :\r\n : 5 1.5 .\r\n : 5 .\r\n : 5 .\r\n : 5 .\r\n HSBC: 5 .\r\n : 5 3 .\r\n QNB: 5 .\r\n : 5 .\r\n : 5 4 .\r\n : 5 ."}]
+
+class NewsResponse {
+  NewsResponse({
+      String? status, 
+      int? totalResults, 
+      List<Articles>? articles,}){
+    _status = status;
+    _totalResults = totalResults;
+    _articles = articles;
+}
+
+  NewsResponse.fromJson(dynamic json) {
+    _status = json['status'];
+    _totalResults = json['totalResults'];
+    if (json['articles'] != null) {
+      _articles = [];
+      json['articles'].forEach((v) {
+        _articles?.add(Articles.fromJson(v));
+      });
+    }
+  }
+  String? _status;
+  int? _totalResults;
+  List<Articles>? _articles;
+
+  String? get status => _status;
+  int? get totalResults => _totalResults;
+  List<Articles>? get articles => _articles;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['status'] = _status;
+    map['totalResults'] = _totalResults;
+    if (_articles != null) {
+      map['articles'] = _articles?.map((v) => v.toJson()).toList();
+    }
+    return map;
+  }
+
+}
+
+/// source : {"id":null,"name":"Youm7.com"}
+/// author : null
+/// title : "أسعار الذهب ترتفع 15 جنيها بالتعاملات المسائية.. وعيار 21 يسجل 1080 جنيها - اليوم السابع"
+/// description : "شهدت أسعار الذهب مستهل التعاملات المسائية اليوم الثلاثاء ارتفاعا ملحوظا بحوالى 15 جنيها للجرام، حيث تحرك جرام الذهب عيار 21 وهو الأكثر مبيعا من 1065 جنيها للجرام إلى 1080 جنيها."
+/// url : "https://www.youm7.com/story/2022/8/16/أسعار-الذهب-ترتفع-15-جنيها-بالتعاملات-المسائية-وعيار-21-يسجل/5873185"
+/// urlToImage : "https://img.youm7.com/large/202203220713151315.jpg"
+/// publishedAt : "2022-08-16T19:00:00Z"
+/// content : null
+
+class Articles {
+  Articles({
+      Source? source, 
+      dynamic author, 
+      String? title, 
+      String? description, 
+      String? url, 
+      String? urlToImage, 
+      String? publishedAt, 
+      dynamic content,}){
+    _source = source;
+    _author = author;
+    _title = title;
+    _description = description;
+    _url = url;
+    _urlToImage = urlToImage;
+    _publishedAt = publishedAt;
+    _content = content;
+}
+
+  Articles.fromJson(dynamic json) {
+    _source = json['source'] != null ? Source.fromJson(json['source']) : null;
+    _author = json['author'];
+    _title = json['title'];
+    _description = json['description'];
+    _url = json['url'];
+    _urlToImage = json['urlToImage'];
+    _publishedAt = json['publishedAt'];
+    _content = json['content'];
+  }
+  Source? _source;
+  dynamic _author;
+  String? _title;
+  String? _description;
+  String? _url;
+  String? _urlToImage;
+  String? _publishedAt;
+  dynamic _content;
+
+  Source? get source => _source;
+  dynamic get author => _author;
+  String? get title => _title;
+  String? get description => _description;
+  String? get url => _url;
+  String? get urlToImage => _urlToImage;
+  String? get publishedAt => _publishedAt;
+  dynamic get content => _content;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    if (_source != null) {
+      map['source'] = _source?.toJson();
+    }
+    map['author'] = _author;
+    map['title'] = _title;
+    map['description'] = _description;
+    map['url'] = _url;
+    map['urlToImage'] = _urlToImage;
+    map['publishedAt'] = _publishedAt;
+    map['content'] = _content;
+    return map;
+  }
+
+}
+
+/// id : null
+/// name : "Youm7.com"
+
+class Source {
+  Source({
+      dynamic id, 
+      String? name,}){
+    _id = id;
+    _name = name;
+}
+
+  Source.fromJson(dynamic json) {
+    _id = json['id'];
+    _name = json['name'];
+  }
+  dynamic _id;
+  String? _name;
+
+  dynamic get id => _id;
+  String? get name => _name;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = _id;
+    map['name'] = _name;
+    return map;
+  }
+
+}
